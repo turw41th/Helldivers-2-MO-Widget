53085e90e1335425f24f2da2cf390d4cd8a1b943
Nicolas Baur <nicolas.baur@dataunit.ch>
2024-05-13 13:55:20 +0200

    Adds basic android app layout

---
+++ app/src/main/java/dev/turwaith/majororderwidget/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.turwaith.majororderwidget
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
